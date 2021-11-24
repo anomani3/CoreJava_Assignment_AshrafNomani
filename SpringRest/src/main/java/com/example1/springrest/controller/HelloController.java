98f5c5ce00c392c5c8d1ed8e0d9d68e0890b3c15
anomani <anomani@LIN25008038.corp.capgemini.com>
2021-11-24 09:35:09 +0530

    Spring Rest First Commit

---
+++ SpringRest/src/main/java/com/example1/springrest/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example1.springrest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hi")
+public String hi()
+{
+	return "helloooo";
+}
+}
